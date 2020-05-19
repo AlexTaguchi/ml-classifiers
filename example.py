e2d84330a279da00221c845514d2718b740b23a6
AlexTaguchi <thatflippingfool@gmail.com>
2020-05-19 16:34:34 -0700

    convert mlm to mlc convention

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 # Fit Tabular Data to Various Machine Learning Classifier Models
 
 # Import modules
-from mlm.data import Preprocess
-from mlm.model import NaiveBayes, Logistic, RandomForest, SVM, NeuralNet
+from mlc.data import Preprocess
+from mlc.model import NaiveBayes, Logistic, RandomForest, SVM, NeuralNet
 import pandas as pd
 
 # Import dataset as dataframe where last column is class label
 # data = pd.read_csv('data/iris.csv', header=None).sample(frac=1)
-data = pd.read_csv('data/skin.csv', header=None, sep='\t').sample(frac=1).iloc[:10000]
-# data = pd.read_csv('data/titanic.csv', header=0).sample(frac=1)
+data = pd.read_csv('data/titanic.csv', header=0).sample(frac=1)
+# data = pd.read_csv('data/skin.csv', header=None, sep='\t').sample(frac=1).iloc[:10000]
 
 # Preprocess dataframe and split into train and test sets
 train, test = Preprocess()(data)
